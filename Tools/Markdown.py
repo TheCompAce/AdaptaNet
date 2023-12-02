f01531aa0e3363512628e6a8fe5ed63d5eeca269
TheCompAce <47639480+TheCompAce@users.noreply.github.com>
2023-12-02 15:21:32 -0500

    PAPER.pdf made

--- Tools/Markdown.py
+++ Tools/Markdown.py
@@ -1,21 +1,24 @@
-import markdown
+import markdown2
+import pdfkit
 import sys
-from weasyprint import HTML
 
-def markdown_to_pdf(md_file_path, pdf_file_path):
-    # Read the Markdown file
-    with open(md_file_path, 'r', encoding='utf-8') as md_file:
-        md_content = md_file.read()
+def markdown_to_pdf(input_file, output_file):
+    # Read Markdown content from a file
+    with open(input_file, 'r', encoding='utf-8') as file:
+        markdown_text = file.read()
 
     # Convert Markdown to HTML
-    html_content = markdown.markdown(md_content)
+    html_text = markdown2.markdown(markdown_text)
 
-    # Convert HTML to PDF and save
-    HTML(string=html_content).write_pdf(pdf_file_path)
+    # Convert HTML to PDF and save the PDF file
+    pdfkit.from_string(html_text, output_file)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python markdown_to_pdf.py [input.md] [output.pdf]")
+        print("Usage: python markdown_to_pdf.py [input_file.md] [output_file.pdf]")
         sys.exit(1)
 
-    markdown_to_pdf(sys.argv[1], sys.argv[2])
+    input_md = sys.argv[1]
+    output_pdf = sys.argv[2]
+    markdown_to_pdf(input_md, output_pdf)
+    print(f"Converted {input_md} to {output_pdf}")
